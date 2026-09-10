2c55eb3ddeaca73d3cc47aa0d034f294de32ecdb
juzer09jawadwala-source <juzer09jawadwala@gmail.com>
2026-09-10 10:24:10 +0530

    Configure images.qualities in next.config.js to eliminate Next.js 16 quality warnings

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    qualities: [70, 75, 80, 85, 90, 95, 100],
     // Site uses remote images from Emergent CDN + Unsplash.
     remotePatterns: [
       { protocol: "https", hostname: "customer-assets.emergentagent.com" },
